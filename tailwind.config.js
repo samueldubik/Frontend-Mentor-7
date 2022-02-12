6f877a3198d5110590e71e1363b7a2e264ae320d
Samuel Dubik <samuel.dubik@gmail.com>
2022-02-12 22:37:18 +0100

    finished

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+
+  },
+    colors: {
+      'cyan': '#19a2ae',
+      'blue': '#2d3248',
+      'gray-blue': '#6a6f81',
+      'gray': '#969696',        
+      },
+
+    fontFamily: { 
+      'custom' : ['Kumbh Sans']
+    },
+  },
+
+  plugins: [],
+}
